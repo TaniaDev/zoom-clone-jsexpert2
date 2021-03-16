e9cec00331bc0c34430d6c446f703c26e9aecea1
Tania de Arruda <tandearruda@gmail.com>
2021-03-16 19:52:48 -0300

    [Dia 1 finalizado] Câmera e conexão funcionando

---
+++ public/pages/room/src/app.js
@@ -0,0 +1,38 @@
+
+
+const recordClick = function (recorderBtn) {
+  this.recordingEnabled = false
+  return () => {
+    this.recordingEnabled = !this.recordingEnabled
+    recorderBtn.style.color = this.recordingEnabled ? 'red' : 'white'
+  }
+}
+
+const onload = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const room = urlParams.get('room');
+  console.log('this is the room', room)
+
+  //const recorderBtn = document.getElementById('record')
+  //recorderBtn.addEventListener('click', recordClick(recorderBtn))
+
+  const socketUrl = 'http://localhost:3000'
+  const socketBuilder = new SocketBuilder ({ socketUrl })
+  const view = new View()
+  const media = new Media()
+  const deps = {
+    view,
+    media,
+    room,
+    socketBuilder
+  }
+
+  Business.initialize(deps)
+
+  //view.renderVideo({ userId : 'test01', url : 'https://media.giphy.com/media/blSTtZehjAZ8I/giphy.mp4'})
+  //view.renderVideo({ userId : 'test01', isCurrentId: true, url : 'https://media.giphy.com/media/blSTtZehjAZ8I/giphy.mp4'})
+ // view.renderVideo({ userId : 'test02', url : 'https://media.giphy.com/media/blSTtZehjAZ8I/giphy.mp4'})
+
+}
+
+window.onload = onload
